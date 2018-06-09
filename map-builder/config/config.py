34c95b517ffad249060cc242f11d4c736f60e83c
Grégoire Weber <gweber@clever-age.com>
2018-06-09 19:08:36 -0400

    Update POI filter method to show signs on map

--- map-builder/config/config.py
+++ map-builder/config/config.py
@@ -3,10 +3,8 @@ def playerIcons(poi):
         poi['icon'] = "https://overviewer.org/avatar/%s" % poi['EntityId']
         return "Last known location for %s" % poi['EntityId']
 
-# Only signs with "-- RENDER --" on the last line will be shown
-# Otherwise, people can't have secret bases and the render is too busy anyways.
 def signFilter(poi):
-    if poi['id'] == 'Sign':
+    if poi['id'] == 'Sign' or poi['id'] == 'minecraft:sign':
         if poi['Text4'] == '-- RENDER --':
             return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
 
